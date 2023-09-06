c65632dc5dfb4e46cf1ef52296563e4b2d1104e7
whonion <playregedit@gmail.com>
2023-09-06 19:37:09 +0800

    push commit dev-stage

--- docker.sh
+++
@@ -1,4 +0,0 @@
-cd $HOME
-md .whonion.app/nginx
-cd .whonion.app/nginx
-docker run --restart unless-stopped --name whonion-app -v ${PWD}:/usr/share/nginx/html -p 80:80 -d --rm nginx
---
+++ public/README.md
@@ -0,0 +1,2 @@
+# <a href="https://whonion.app">whonion.app</a>
+<p>Our service's Landing page V.0.108</p>
